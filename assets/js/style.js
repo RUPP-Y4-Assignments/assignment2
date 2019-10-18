88bb3269c0b061caf04041390bf71327d0865449
ruppsenglymeng <fightUptogether@gmail.com>
2019-10-18 17:24:05 +0700

    blog and event

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,22 +2,40 @@
 
 
 $(document).ready(function () {
+    // var btn_menu = $('#btn-menu');
+    // var class_left_side = $('.fixed.left-side>#krb-aside');
+    // var center_side = $('.center-side'), clicked = 0;
+    // btn_menu.click(function () {
+    //     // class_left_side.css({"display":"block"})
+
+    //     if (clicked == 0) {
+    //         class_left_side.css({'width':'70%','overflow-y':'scroll'})
+    //         class_left_side.show()
+    //         center_side.css({'left':'70%','width': '30%' ,'transition':'0.5s'})
+    //         clicked = 1;
+    //     } else {
+    //         class_left_side.hide()
+    //         center_side.css({ 'left': '0', 'width': '100%' })
+    //         clicked = 0;
+    //     }
+
+    // })
     var btn_menu = $('#btn-menu');
-    var class_left_side = $('.fixed.left-side>#krb-aside');
+    var class_left_side = $('.left-side');
+    // var class_left_side = $('.fixed .left-side');
     var center_side = $('.center-side'), clicked = 0;
+
     btn_menu.click(function () {
-        // class_left_side.css({"display":"block"})
 
         if (clicked == 0) {
-            class_left_side.css({'width':'70%','overflow-y':'scroll'})
-            class_left_side.show()
-            center_side.css({'left':'70%','width': '30%' ,'transition':'0.5s'})
+            class_left_side.css({'left':'0','transition':'0.5s'});
+            center_side.css({'left':'50%','transition':'0.5s'});
             clicked = 1;
         } else {
-            class_left_side.hide()
-            center_side.css({ 'left': '0', 'width': '100%' })
+            class_left_side.css({'left':'-50%','transition':'0.5s'});
+            center_side.css({ 'left': '0','transition':'0.5s'});
             clicked = 0;
         }
 
-    })
-})
+    });
+});
